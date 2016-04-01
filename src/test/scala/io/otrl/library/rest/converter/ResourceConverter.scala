ada44df7e65372229f65fe2c01979d0e41a34a45
Edin Hodzic <edin@edinhodzic.co.uk>
2016-04-01 09:12:44 +0100

    tidy up by removing use of 'new' keyword when instantiating case classes

--- src/test/scala/io/otrl/library/rest/converter/ResourceConverter.scala
+++ src/test/scala/io/otrl/library/rest/converter/ResourceConverter.scala
@@ -8,6 +8,6 @@ class ResourceConverter extends Converter[Resource, HttpEntity] {
 
   override def serialise(subject: Resource): HttpEntity = HttpEntity( """{ "data" : "value" } """)
 
-  override def deserialise(subject: HttpEntity): Resource = new Resource("value")
+  override def deserialise(subject: HttpEntity): Resource = Resource("value")
 
 }
